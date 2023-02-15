e0dd80a3e77dfd489e5bdf0064fd2d38bd7eb70c
gaurav20619 <gauravsinghraijada619@gmail.com>
2023-02-15 07:21:33 +0530

    Add: Making start position zero for snake and ladder program.

---
+++ src/SnakeAndLadder.java
@@ -0,0 +1,8 @@
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        //UC1 starting position is 0
+                System.out.println("Welcome to Snake and Ladder Game");
+                int position =0;
+            }
+        
+}

8b31601fac1d59274a65e0c017e71a04c4ceb623
gaurav20619 <gauravsinghraijada619@gmail.com>
2023-02-15 07:29:18 +0530

    Add: option for no play,ladder or snake.

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,9 +1,24 @@
 public class SnakeAndLadder {
     public static void main(String[] args) {
-		
-		System.out.println("Welcome to Snake and Ladder Game");
-		int position =0;
-		int randomCheck = (int) Math.floor(Math.random ()* 10) % 6+1;
-		System.out.println(randomCheck);
-	}
+        //UC3-randomly checking for option no play, ladder or snake
+                
+                System.out.println("Welcome to Snake and Ladder Game");
+                int position =0;
+                int randomCheck = (int) Math.floor(Math.random ()* 10) % 6+1;
+                int randomCheck2 = (int) (Math.random()* 10 )% 3;
+                switch (randomCheck2) {
+                    case 0:
+                        System.out.println("No Play");
+                        position +=0;
+                        break;
+                    case 1:
+                        System.out.println("Ladder");
+                        position +=randomCheck;
+                        break;
+                    case 2:
+                        System.out.println("Snake");
+                        position -=randomCheck;
+                        break;
+                }
+            }
 }
